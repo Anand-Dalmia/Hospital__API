7e3fc7fba56487020203db276fe2269b5422e804
Anand-Dalmia <adanandkumar12@gmail.com>
2023-07-12 23:27:02 +0530

    Second changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 //connect to DB
-mongoose.connect('mongodb://127.0.0.1/hospital_API', { useNewUrlParser: true ,useUnifiedTopology: true });
+mongoose.connect(process.env.db, { useNewUrlParser: true ,useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to db"));
